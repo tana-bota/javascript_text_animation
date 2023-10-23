99a1f1c96af53c09e6b5d350d4d07945ec0d22fa
tana-bota <107912289+tana-bota@users.noreply.github.com>
2023-10-23 15:12:15 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,75 @@
+// JavaScript
+// フェード
+const fade = document.querySelector('.fade');
+
+const fadeKeyframes = {
+  opacity: [0, 1],
+}
+
+const fadeOptions = {
+  duration: 3000,
+  easing: 'ease',
+  //fill: 'forwards',
+  direction: 'alternate',
+  iterations: Infinity,
+}
+
+fade.animate(fadeKeyframes, fadeOptions);
+
+//duration　3000なら3秒
+
+// 回転
+const spin = document.querySelector('.spin');
+
+const spinKeyframes = {
+  rotate: ['x 360deg','x 0deg'],
+  opacity: [0, 1],
+}
+
+const spinOptions = {
+  duration: 2000,
+  easing: 'ease',
+}
+spin.animate(spinKeyframes, spinOptions);
+// 色の変更
+const color = document.querySelector('.color');
+
+const colorKeyframes = {
+  backgroundColor: ['lightgreen','slyblue','bule ','purple '],
+}
+
+const colorOptions = {
+  duration: 5000,
+  direction: 'alternate',
+  iterations: Infinity,
+}
+
+color.animate(colorKeyframes,colorOptions);
+// 延びる背景
+const line = document.querySelector('.line');
+
+const lineKeyframes = {
+  width: ['0','100%'],
+  color: ['transparent','#FFF'],
+}
+
+const lineOptions = {
+  duration: 2000,
+  fill: 'forwards',
+}
+
+line.animate(lineKeyframes,lineOptions);
+// スライドイン
+const slide = document.querySelector('.slide-in');
+
+const slideKeyframes = {
+  opacity: [0, 1],
+  translate: ['0 50px', 0],
+};
+
+const slideOptions = {
+  duration: 1000,
+  fill: 'forwards',
+};
+
+slide.animate(slideKeyframes,slideOptions);
